bff89ba6028889860e542fef659d6c4a8dd2ca1c
SeokjinDev <tjrwls1108011@gmail.com>
2024-11-04 03:57:16 +0900

    [FIX] Agent와 통신 문제 해결

--- agent/src/main/java/com/capstone/agent/api/agent/controller/AgentController.java
+++ agent/src/main/java/com/capstone/agent/api/agent/controller/AgentController.java
@@ -1,33 +1,35 @@
 package com.capstone.agent.api.agent.controller;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.capstone.agent.api.agent.dto.ChatLogDTO;
+import com.capstone.agent.api.agent.dto.QuestionDTO;
 import com.capstone.agent.api.agent.entity.Role;
+import com.capstone.agent.api.agent.service.AgentService;
 import com.capstone.agent.api.agent.service.HistoryService;
 import com.capstone.agent.api.member.dto.MemberInfoResponseDTO;
 import com.capstone.agent.api.member.jwt.service.JwtService;
 import com.capstone.agent.api.member.service.MemberService;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import jakarta.servlet.http.HttpServletRequest;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URLEncoder;
-import java.util.Map;
 
 @Slf4j
 @RestController
@@ -39,10 +41,11 @@ public class AgentController {
     private final MemberService memberService;
     private final HistoryService historyService;
     private final JwtService jwtService;
+    private final AgentService agentService;
 
     @GetMapping("/question")
     @ResponseBody
-    public ResponseEntity<?> userQeustion(@RequestParam String query, HttpServletRequest request) {
+    public HashMap<String, String> userQeustionTest(@RequestParam String query, HttpServletRequest request) {
         String accessToken = jwtService.extractAccessToken(request)
                 .filter(jwtService::isTokenValid)
                 .orElseThrow(() -> new RuntimeException("유효하지 않은 Access Token"));
@@ -54,49 +57,35 @@ public class AgentController {
         log.info("User ID: {}, Role: {}, Query: {}", memberInfo.getId(), memberInfo.getRole(), query);
 
         // question log 저장
-        ChatLogDTO chatLogDTO = ChatLogDTO.builder()
+        ChatLogDTO userChatLogDTO = ChatLogDTO.builder()
                 .memberId(memberInfo.getId())
                 .log(query)
                 .role(Role.HUMAN)
                 .build();
-        historyService.saveLog(chatLogDTO);
+        historyService.saveLog(userChatLogDTO);
 
         try {
-            HttpHeaders headers = new HttpHeaders();
-            String encodedQuery = URLEncoder.encode(query, "UTF-8");
-            Long memberId = memberInfo.getId();
-            String encodedAddr = URLEncoder.encode(memberInfo.getAddr(), "UTF-8");
-            String encodedGender = URLEncoder.encode(memberInfo.getGender().toString(), "UTF-8");
-            int age = memberInfo.getAge();
-            String encodedParams = String.format("?query=%s&id=%d&loc=%s&gender=%s&age=%d", encodedQuery, memberId, encodedAddr, encodedGender, age);
-            String agentPort = "5000";
-            String newURL = String.format(
-                    "%s://%s:%s/agent/question%s",
-                    request.getScheme(),
-                    request.getServerName(),
-                    agentPort,
-                    encodedParams );
-            headers.setLocation(URI.create(newURL));
-            return new ResponseEntity<>(headers, HttpStatus.MOVED_PERMANENTLY);
-        } catch (UnsupportedEncodingException e) {
-            log.error(email, e);
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+            QuestionDTO question = QuestionDTO.builder()
+                    .query(query)
+                    .id(memberInfo.getId())
+                    .location(memberInfo.getAddr())
+                    .gender(memberInfo.getGender().toString())
+                    .age(memberInfo.getAge())
+                    .build();
+            HashMap<String, String> answer = agentService.getAgentQuestionHash(question);
 
-    @GetMapping("/answer")
-    public Map<String, String> agnetAnswer(@RequestBody Map<String, String> json, HttpServletRequest request) {
-        log.info("User ID: {}, Role: Agent, Query: {}", json.get("id"), json.get("answer"));
-        
         // question log 저장
-        ChatLogDTO chatLogDTO = ChatLogDTO.builder()
-                .memberId(Long.parseLong(json.get("id")))
-                .log(json.get("answer"))
+        ChatLogDTO agentChatLogDTO = ChatLogDTO.builder()
+                .memberId(memberInfo.getId())
+                .log(answer.get("answer"))
                 .role(Role.COMPUTER)
                 .build();
-        historyService.saveLog(chatLogDTO);
-
-        return json;
+        historyService.saveLog(agentChatLogDTO);
+            
+            return answer;
+        } catch (Exception e) {
+            log.error(email, e);
+            return new HashMap<>();
+        }
     }
-
 }
---
+++ agent/src/main/java/com/capstone/agent/api/agent/service/AgentService.java
@@ -0,0 +1,57 @@
+package com.capstone.agent.api.agent.service;
+
+import java.util.HashMap;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.capstone.agent.api.agent.dto.QuestionDTO;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AgentService {
+    private final WebClient webClient;
+
+    public String getAgentQuestion(QuestionDTO question) {
+        String response = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .scheme("http")
+                        .host("localhost")
+                        .port(5001)
+                        .path("/agent/question_test")
+                        .queryParam("query", question.getQuery())
+                        .queryParam("id", question.getId())
+                        .queryParam("loc", question.getLocation())
+                        .queryParam("gender", question.getGender())
+                        .queryParam("age", question.getAge())
+                        .build())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        return response;
+    }
+
+    public HashMap<String, String> getAgentQuestionHash(QuestionDTO question) {
+        HashMap<String, String> response = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .scheme("http")
+                        .host("localhost")
+                        .port(5001)
+                        .path("/agent/question_test")
+                        .queryParam("query", question.getQuery())
+                        .queryParam("id", question.getId())
+                        .queryParam("loc", question.getLocation())
+                        .queryParam("gender", question.getGender())
+                        .queryParam("age", question.getAge())
+                        .build())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<HashMap<String, String>>() {})
+                .block();
+        return response;
+    }
+}
